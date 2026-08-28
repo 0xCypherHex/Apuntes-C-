1097779f848b761804247fd5c8c33f27250e286c
Usuario administrador <machine101@Blouse.ipv6.infinitum.net.mx>
2026-08-28 12:42:55 -0600

    Actualizacion practica 4

--- Programacion en C/practica4.c
+++ Programacion en C/practica4.c
@@ -1,1 +1,20 @@
 #include<stdio.h>
+
+int main() {
+
+    int a=0,b=0;
+
+    // Entrada de a y b
+
+    printf("Teclea valor de a: ");
+    scanf("%i", &a);
+
+    printf("Teclea valor de b: ");
+    scanf("%i", &b);
+
+    // Salida
+
+    printf("Valor de a= %i\n Valor de b= %i\n", a,b);
+
+    return 0;
+}
