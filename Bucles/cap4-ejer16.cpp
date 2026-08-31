a398cfce3439a0b6d4e7492dbcb9c8a1e46e4c39
Usuario administrador <machine101@Blouse.ipv6.infinitum.net.mx>
2026-08-31 13:05:47 -0600

    Ejercicio cap4-ejer16

---
+++ Bucles/cap4-ejer16.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdlib>
+
+int main() {
+
+    
+    int iCantidadPersonas=0;
+    int iSumaEdades=0; 
+    float fPromedioEdades=0;
+
+    int iEdad=0;
+
+    int i=1;
+
+    std::cout << "Digite la cantidad de personas: ";
+        std::cin >> iCantidadPersonas;
+
+    
+    
+    do {
+
+        std::cout << "Ingrese la edad de la persona " << i << ": ";
+            std::cin >> iEdad;
+            
+        iSumaEdades += iEdad;
+
+        i++;
+
+    } while (i <= iCantidadPersonas);
+
+    fPromedioEdades = iSumaEdades / iCantidadPersonas;
+
+        std::cout << "Suma edades: " << iSumaEdades << std::endl << "Promedio edades: " << fPromedioEdades << std::endl;
+
+
+    return 0;
+}
