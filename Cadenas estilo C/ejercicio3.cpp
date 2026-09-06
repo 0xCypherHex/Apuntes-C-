36e8db4e22def564b1bba6facc1d96f85507ff03
Kilo <delta@hikari.ipv6.infinitum.net.mx>
2026-09-06 12:47:04 -0600

    Creacion 'Cadena estilo C'/ejercicio3.cpp

---
+++ Cadenas estilo C/ejercicio3.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+
+// Funcion para concatenar dos cadenas
+
+std::string concatenateStrings(string str1, std::string str2){
+
+    std::string result = str1 + " " + str2;
+
+    return result;
+}
+
+
+
+int main() {
+
+    std::string firstName;
+    std::string lastName;
+
+    std::string fullName;
+
+    std::cout << "Inserte su nombre: ";
+        std::getline(std::cin, firstName);
+
+    std::cout << "Inserte sus apellidos: ";
+        std::getline(std::cin, lastName);
+
+    fullName = concatenateStrings(firstName, lastName);
+    
+    std::cout << fullName << std::endl;
+
+    // Longitud de una cadena string se usa .length() o .size()
+
+    int lenghtFullName = fullName.length();
+
+    std::cout << "Longitud del nombre: " << lenghtFullName << std::endl;
+
+
+    return 0;
+}
