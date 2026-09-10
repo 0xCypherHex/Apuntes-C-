728c9e228a4600f461fa89933041cb4b810759f4
Usuario administrador <machine101@Blouse.ipv6.infinitum.net.mx>
2026-09-10 11:17:01 -0600

    Creacion 'Programas en C'/switch.c

---
+++ Programacion en C/switch.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int iOpcion = 0;
+
+    printf("--- Menú Principal ---\n");
+    printf("1. Consultar Saldo\n2. Depositar\n3. Retirar\nSeleccione una opción: ");
+    scanf("%d", &iOpcion);
+
+    switch (iOpcion) {
+        case 1:
+            printf("Su saldo actual es de $1,000.00\n");
+            break;
+        case 2:
+            printf("Depósito realizado con éxito.\n");
+            break;
+        case 3:
+            printf("Retiro procesado correctamente.\n");
+            break;
+        default:
+            printf("Opción inválida. Intente de nuevo.\n");
+            break;
+    }
+
+    return 0;
+}
