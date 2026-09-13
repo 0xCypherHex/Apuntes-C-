8574cb6b7f9ffe63dd725e1b134c129729ee73c3
0xCypherHex <fallaut478@gmail.com>
2026-09-13 17:27:03 -0600

    actualizacion 'Programacion en C'/clasificadorCompletoDeTriangulos.cpp

--- Programacion en C/clasificadorCompletoDeTriangulos.c
+++ Programacion en C/clasificadorCompletoDeTriangulos.c
@@ -3,28 +3,16 @@
 
 void clasificador(float fAngulo1, float fAngulo2, float fAngulo3){
 
- if (fAngulo1 == 60 && fAngulo2 == 60 && fAngulo3 == 60) {
-        
+    if (fAngulo1 == 60 && fAngulo2 == 60 && fAngulo3 == 60) {
         printf("Y ademas es equilatero. \n");
-
-        
-        } 
+    }
     else if (fAngulo1 != fAngulo2 && fAngulo2 != fAngulo3 && fAngulo1 != fAngulo3) {
-
         printf("Y ademas es escaleno. \n");
-    
-
-        }
-    
-    else  {
-
+    }
+    else {
         printf("Y ademas es isosceles. \n");
-
-        }
-
-
+    }
 }
-
 
 int main (void) {
 
@@ -36,7 +24,7 @@ printf("Ingrese el primer angulo: ");
     scanf("%f", &fPrimerAngulo);
 
 // Validar datos primer angulo
-if (fPrimerAngulo < 0 || fPrimerAngulo > 180){
+if (fPrimerAngulo <= 0 || fPrimerAngulo >= 180){
     
     printf("Dato invalido. \n");
     return 0;
@@ -49,7 +37,7 @@ printf("Ingrese el segundo angulo: ");
     scanf("%f", &fSegundoAngulo);
 
 // Validar datos segundo angulo
-if (fSegundoAngulo < 0 || fSegundoAngulo > 180){
+if (fSegundoAngulo <= 0 || fSegundoAngulo >= 180){
 
     printf("Dato invalido. \n");
     return 0;
@@ -60,6 +48,14 @@ if (fSegundoAngulo < 0 || fSegundoAngulo > 180){
 // se ingresa primero 180 para que el resultado sea positivo
 float fTercerAngulo = 180 - (fPrimerAngulo + fSegundoAngulo); 
 
+// Validar datos tercer angulo
+if (fTercerAngulo <= 0 || fTercerAngulo >= 180){
+
+    printf("Dato invalido. \n");
+    return 0;
+
+}
+
 // Comprobar que la suma de los angulos den 180
 float fComprobacionSumaDeAngulos = fPrimerAngulo + fSegundoAngulo + fTercerAngulo;
 
@@ -68,43 +64,32 @@ if (fComprobacionSumaDeAngulos != 180){
     printf("Error en la validacion de datos. \n");
     return 0;
 
-    }
+}
 
 // Clasificar angulos con condiciones anidadas
-
-// Triangulo Rectangulo
 if (fPrimerAngulo == 90 || fSegundoAngulo == 90 || fTercerAngulo == 90){
 
     printf("El triangulo es rectangulo. \n");
-    
     clasificador(fPrimerAngulo, fSegundoAngulo, fTercerAngulo);
-
-    return 0;
-
-    } 
-
-// Triangulo Obstusangulo
-if (fPrimerAngulo > 90 || fSegundoAngulo > 90 || fTercerAngulo > 90){
+}
+    
+else if (fPrimerAngulo > 90 || fSegundoAngulo > 90 || fTercerAngulo > 90){
 
     printf("El triangulo es obtusangulo. \n");
-   
     clasificador(fPrimerAngulo, fSegundoAngulo, fTercerAngulo);
-
-    return 0;
-      
-    }
-
-// Triangulo Actutangulo
-if (fPrimerAngulo < 90 && fSegundoAngulo < 90 && fTercerAngulo < 90) {
+}
+    
+else if (fPrimerAngulo < 90 && fSegundoAngulo < 90 && fTercerAngulo < 90) {
 
     printf("El triangulo es acutangulo. \n");
-
     clasificador(fPrimerAngulo, fSegundoAngulo, fTercerAngulo);
+}
+    
+    else {
+    
+        printf("No se pudo clasificar el triangulo. \n");
 
-    return 0;
+    }
 
-    }     
-
-
-    return 0;
+return 0;
 }
