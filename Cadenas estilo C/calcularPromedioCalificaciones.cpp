f7c3d2bfa169d4c204c4ee4b64f97dfc764875f1
0xCypherHex <fallaut478@gmail.com>
2026-09-13 21:19:50 -0600

    creacion 'Cadenas estilo C'/calcularPromedioCalificaciones.cpp

--- Cadenas estilo C/calcularPromedioCalificaciones.cpp
+++ Cadenas estilo C/calcularPromedioCalificaciones.cpp
@@ -1,14 +1,39 @@
 #include<iostream>
-#include<vector>
 
 double calcularPromedioCal(int calificaciones[], int size){
 
+    double suma = 0.0;
+
+    for (int i = 0; i < size; i++) {
+        suma += calificaciones[i];
+    }
+
+    double promedio = suma/size;
+
+   return promedio;
 
 }
 
 
-
 int main(void) {
+
+    int cantidadCalificaciones = 0;
+    std::cout << "Introduzca cantidad de calificaciones: ";
+        std::cin >> cantidadCalificaciones;
+
+    int calificaciones[cantidadCalificaciones];
+    
+    for (int i = 0; i < cantidadCalificaciones; i++){ // Almacenar calificaciones en un array
+
+        int val = 0;
+        std::cout << "Inserte calificacion numero " << i << ": ";
+             std::cin >> val;
+        calificaciones[i] = val;
+
+    } 
+
+    double promedioCalificaciones = calcularPromedioCal(calificaciones, cantidadCalificaciones);
+    std::cout << "Promedio: " << promedioCalificaciones << std::endl;
 
     return 0;
 }
