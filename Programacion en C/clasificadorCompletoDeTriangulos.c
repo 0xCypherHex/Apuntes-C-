034d600927df57e1e156a32fbb5c92f9e0627ab0
Kilo <delta@hikari.ipv6.infinitum.net.mx>
2026-09-13 18:21:50 -0600

    Actualizacion 'Programacion en C'/clasificadorCompletoDeTriangulos.cpp

--- Programacion en C/clasificadorCompletoDeTriangulos.c
+++ Programacion en C/clasificadorCompletoDeTriangulos.c
@@ -6,9 +6,11 @@ void clasificador(float fAngulo1, float fAngulo2, float fAngulo3){
     if (fAngulo1 == 60 && fAngulo2 == 60 && fAngulo3 == 60) {
         printf("Y ademas es equilatero. \n");
     }
+   
     else if (fAngulo1 != fAngulo2 && fAngulo2 != fAngulo3 && fAngulo1 != fAngulo3) {
         printf("Y ademas es escaleno. \n");
     }
+   
     else {
         printf("Y ademas es isosceles. \n");
     }
@@ -70,19 +72,25 @@ if (fComprobacionSumaDeAngulos != 180){
 if (fPrimerAngulo == 90 || fSegundoAngulo == 90 || fTercerAngulo == 90){
 
     printf("El triangulo es rectangulo. \n");
+
     clasificador(fPrimerAngulo, fSegundoAngulo, fTercerAngulo);
+
 }
     
 else if (fPrimerAngulo > 90 || fSegundoAngulo > 90 || fTercerAngulo > 90){
 
     printf("El triangulo es obtusangulo. \n");
+
     clasificador(fPrimerAngulo, fSegundoAngulo, fTercerAngulo);
+
 }
     
 else if (fPrimerAngulo < 90 && fSegundoAngulo < 90 && fTercerAngulo < 90) {
 
     printf("El triangulo es acutangulo. \n");
+
     clasificador(fPrimerAngulo, fSegundoAngulo, fTercerAngulo);
+
 }
     
     else {
