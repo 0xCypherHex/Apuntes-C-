a0d71ebb4878e8c7a7983e0827daef00fd1774ca
0xCypherHex <fallaut478@gmail.com>
2026-09-15 20:10:48 -0600

    Examen

---
+++ examen.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void) {
+
+    int num1 = 10 + 5 * 2;
+
+    printf("%d\n", num1);
+
+    if((8 != 8) || (5 > 2)) {
+
+        printf("1\n");
+    }
+
+    return 0;
+}
